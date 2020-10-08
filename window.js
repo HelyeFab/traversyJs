a9ca0f29f1375d2e01ec172c56073b07ad6cbb71
CodeLeviosa <emmanuelfabiani23@gmail.com>
2020-10-08 18:02:59 +0100

    added file

---
+++ window.js
@@ -0,0 +1,19 @@
+// Confirm
+// if (confirm('Are you sure')){
+//     console.log('yes');
+// } else {
+//     console.log('no');
+// }
+
+
+let val;
+val = window.outerHeight;
+console.log(val);
+
+
+ 
+
+
+
+
+
