2410f8b1700aa71ff73b69fc90f1994665cdb967
CodeLeviosa <emmanuelfabiani23@gmail.com>
2020-10-09 12:15:31 +0100

    added new file

--- domManipulation/app.js
+++ domManipulation/app.js
@@ -1,40 +1,46 @@
 let val;
+// ? HTML collection is formatted as an array but you cannot use forEach:
+//      Even though it's not an array you can still access individual items by their index -> val.all[3]
+//      or use some array properties -> val.all.length;
+// ? NODE collection you can use forEach
+
+
 
 val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// val = document.all;
+// val = document.all[2];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList[0];
 
-val = document.images;
+// val = document.images;
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
 
-let scripts = document.scripts;
+// let scripts = document.scripts;
 
-let scriptsArr = Array.from(scripts);
+// let scriptsArr = Array.from(scripts);
 
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
-});
+// scriptsArr.forEach(function(script) {
+//   console.log(script.getAttribute('src'));
+// });
 
-console.log(val);
+console.log(val.all);
