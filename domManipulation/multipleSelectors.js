1b1a33d5282139621d142d045427b7ebf609edee
CodeLeviosa <emmanuelfabiani23@gmail.com>
2020-10-09 14:48:16 +0100

    added some more js stuff

--- domManipulation/multipleSelectors.js
+++ domManipulation/multipleSelectors.js
@@ -1,3 +1,39 @@
-const items = document.getElementsByClassName('collection-item');
-console.log(items);
-console.log(items[3]);
+// const items = document.getElementsByClassName('collection-item');
+// // console.log(items);
+// // console.log(items[3]);
+
+// const ul = document.querySelector('ul');
+// const items_1 = ul.getElementsByClassName('collection-item');
+// console.log(ul);
+// console.log(items_1);
+
+
+
+// let lis = document.getElementsByTagName('li');
+// // console.log(lis);
+// lis = Array.from(lis);
+
+// lis.forEach(function (li, index){
+//     li.textContent = `Item ${index +1}`
+// });
+
+let lis = document.querySelectorAll('li');
+console.log(lis);
+
+lis.forEach(function (li, index) {
+    li.textContent = `Item ${index + 1}`
+});
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+
+liOdd.forEach(function (li, index) {
+    li.style.background = '#333';
+    li.style.color = '#eee';  
+});
+
+liEven.forEach(function (li, index) {
+    li.style.background = '#e63946';
+    li.style.color = '#eee';
+});
